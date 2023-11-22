677c469e6b2c07bfe5d9f1fc07532cced6e0d258
Alejandro <alejandro01.av@gmail.com>
2023-11-22 17:38:33 +0100

    yfin

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
-import pandas as pd
-import requests
-
+import yfinance as yf
 
 app = dash.Dash(__name__)
-
-API_KEY = '51ZCQSNHYPT65QDB'
 
 top_50_sp500 = [
     {'label': 'Apple Inc', 'value': 'AAPL'},
@@ -18,19 +14,10 @@ top_50_sp500 = [
     {'label': 'Alphabet Inc (Google)', 'value': 'GOOGL'}
 ]
 
-
-
 def fetch_stock_data(symbol):
-    API_KEY = 'YourAlphaVantageAPIKey'
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}&datatype=json'
-    response = requests.get(url)
-    data = response.json()
-    df = pd.DataFrame(data['Time Series (Daily)']).transpose()
-    df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
-    df.index = pd.to_datetime(df.index)
-    df = df.astype({'Open': 'float', 'High': 'float', 'Low': 'float', 'Close': 'float', 'Volume': 'int'})
+    stock = yf.Ticker(symbol)
+    df = stock.history(period="1y")  # You can change the period as needed
     return df
-
 
 app.layout = html.Div(children=[
     html.H1(children='S&P 500 Stock App'),
@@ -53,7 +40,6 @@ def update_graph(selected_stock):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df_stock.index, y=df_stock['Close'], mode='lines', name=f'{selected_stock} Close Price'))
     return fig
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
